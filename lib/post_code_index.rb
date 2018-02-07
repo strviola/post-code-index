23ff9d38429ebfdd4e71cee3dd126659615efe4f
鈴木竜太 <django.contrib.auth.models@gmail.com>
2018-02-07 23:19:27 +0900

    implement dividing n-gram array

--- lib/post_code_index.rb
+++ lib/post_code_index.rb
@@ -1,2 +1,8 @@
 module PostCodeIndex
+  def n_gram_array(string, n = 2)
+    last = string.size - 1
+    (0..last).map do |i|
+      string[i..[(i + n - 1), last].min]
+    end
+  end
 end
--- spec/post_code_index_spec.rb
+++ spec/post_code_index_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe PostCodeIndex do
-  it { expect(true).to eq true }
+  describe %q<n_gram_array('abcbcd', n)> do
+    subject { n_gram_array('abcbcd', n) }
+    context 'n = 2' do
+      let(:n) { 2 }
+      it { is_expected.to match_array %w(ab bc cb bc cd d) }
+    end
+    context 'n = 3' do
+      let(:n) { 3 }
+      it { is_expected.to match_array %w(abc bcb cbc bcd cd d) }
+    end
+  end
 end
