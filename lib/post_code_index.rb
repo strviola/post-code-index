e0a0b7340406771b25017af1a52fecccc4a88586
鈴木竜太 <django.contrib.auth.models@gmail.com>
2018-02-08 22:19:51 +0900

    zip file download method

--- lib/post_code_index.rb
+++ lib/post_code_index.rb
@@ -1,8 +1,24 @@
+require 'open-uri'
+
 module PostCodeIndex
   def n_gram_array(string, n = 2)
     last = string.size - 1
     (0..last).map do |i|
       string[i..[(i + n - 1), last].min]
     end
+  end
+
+  def download_post_code_zip
+    url = 'http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip'
+    open(url) do |remote|
+      File.open(cache_dir('ken_all.zip'), 'wb') do |local|
+        local.puts remote.read
+      end
+    end
+    true
+  end
+
+  def cache_dir(file_name)
+    File.expand_path("../cache/#{file_name}", __FILE__)
   end
 end
