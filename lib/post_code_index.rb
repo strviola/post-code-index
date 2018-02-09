fae6c3481b392abd737e25525ddf4ec3bd8d6fe1
鈴木竜太 <django.contrib.auth.models@gmail.com>
2018-02-09 20:57:16 +0900

    n-gram hash division method

--- lib/post_code_index.rb
+++ lib/post_code_index.rb
@@ -5,4 +5,19 @@ module PostCodeIndex
       string[i..[(i + n - 1), last].min]
     end
   end
+
+  def n_gram_record_hash(post_code_array, n = 2)
+    post_code_index = 2
+    pref_index = 6
+    city_index = 7
+    town_index = 8
+    record_hash = {}
+    [pref_index, city_index, town_index].each do |index|
+      name = post_code_array[index]
+      n_gram_array(name, n).each do |key_word|
+        record_hash[key_word] = post_code_array[post_code_index]
+      end
+    end
+    record_hash
+  end
 end
--- spec/post_code_index_spec.rb
+++ spec/post_code_index_spec.rb
@@ -12,4 +12,26 @@ describe PostCodeIndex do
       it { is_expected.to match_array %w(abc bcb cbc bcd cd d) }
     end
   end
+
+  describe 'n_gram_record_hash' do
+    subject { n_gram_record_hash(post_code_array) } # n = 2
+    let(:post_code_array) do
+      [
+        13104, '169  ', '1690074', 'トウキョウト', 'シンジュクク', 'キタシンジュク',
+        '東京都', '新宿区', '北新宿', 0, 0, 1, 0, 0, 0
+      ]
+    end
+    it 'convert n-gram keyword to post-code hash' do
+      is_expected.to eq({
+        '東京' => '1690074',
+        '京都' => '1690074',
+        '都' => '1690074',
+        '新宿' => '1690074',
+        '宿区' => '1690074',
+        '区' => '1690074',
+        '北新' => '1690074',
+        '宿' => '1690074',
+      })
+    end
+  end
 end
