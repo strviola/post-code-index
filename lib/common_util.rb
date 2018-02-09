cbd0f0ef80f587dcf9954cf2144704a798e87849
鈴木竜太 <django.contrib.auth.models@gmail.com>
2018-02-09 20:59:47 +0900

    isolate constants to util

--- lib/common_util.rb
+++ lib/common_util.rb
@@ -1,4 +1,9 @@
 module CommonUtil
+  POST_CODE_INDEX = 2
+  PREF_INDEX = 6
+  CITY_INDEX = 7
+  TOWN_INDEX = 8
+
   def cache_dir(file_name)
     File.expand_path("../cache/#{file_name}", __FILE__)
   end
--- lib/post_code_index.rb
+++ lib/post_code_index.rb
@@ -1,3 +1,6 @@
+require_relative './common_util'
+include CommonUtil
+
 module PostCodeIndex
   def n_gram_array(string, n = 2)
     last = string.size - 1
@@ -7,15 +10,11 @@ module PostCodeIndex
   end
 
   def n_gram_record_hash(post_code_array, n = 2)
-    post_code_index = 2
-    pref_index = 6
-    city_index = 7
-    town_index = 8
     record_hash = {}
-    [pref_index, city_index, town_index].each do |index|
+    [PREF_INDEX, CITY_INDEX, TOWN_INDEX].each do |index|
       name = post_code_array[index]
       n_gram_array(name, n).each do |key_word|
-        record_hash[key_word] = post_code_array[post_code_index]
+        record_hash[key_word] = post_code_array[POST_CODE_INDEX]
       end
     end
     record_hash
